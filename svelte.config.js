3c0baa059d7acfc1e13e6610147e3a72cfa4a76f
Susanna <s00tangg@gmail.com>
2022-06-18 16:23:43 -0700

    城、

--- svelte.config.js
+++ svelte.config.js
@@ -8,3 +8,26 @@ const config = {
 };
 
 export default config;
+
+// import adapter from "@sveltejs/adapter-static"; 
+// // was "@sveltejs/adapter-auto"
+
+// const dev = "production" === "development";
+
+// /** @type {import(""@sveltejs/kit").Config} */
+// const config = {
+//     kit: {
+//         adapter: adapter({
+//             pages: "docs",
+//             assets: "docs"
+//         }),
+//         paths: {
+//             // change below to your repo name
+//             base: dev ? """ : "/your-repo-name",
+//         },
+//         // hydrate the <div id="svelte"> element in src/app.html
+//         target: "#svelte"
+//     }
+// };
+
+// export default config;
